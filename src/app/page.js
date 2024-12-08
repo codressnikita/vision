ca3620c3ab430c8e51f4483a24d7d2a366ec0948
Shekhar Suman <shekhar.suman1@in.ey.com>
2024-12-09 01:17:26 +0530

    periodic calling of playlist added

--- src/app/page.js
+++ src/app/page.js
@@ -10,37 +10,46 @@ export default function Page() {
   const PLAYLIST_ID = "PL7FyP3s7bLbIpKlZ1EMF_fTooJ--fpkgU";
   const API_KEY = "AIzaSyBHSv6fT10HfnWHE1NoZhP0j_rU3QV385I";
 
+  const fetchVideos = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/youtube/v3/playlistItems",
+        {
+          params: {
+            part: "snippet",
+            playlistId: PLAYLIST_ID,
+            maxResults: 50,
+            key: API_KEY,
+          },
+        }
+      );
+
+      const videosData = response.data.items.map((item) => ({
+        name: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.high.url,
+        src: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
+      }));
+
+      setVideos(videosData);
+    } catch (error) {
+      console.error(
+        "Failed to fetch videos:",
+        error.response?.data || error.message
+      );
+    }
+  };
+
   useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        const response = await axios.get(
-          "https://www.googleapis.com/youtube/v3/playlistItems",
-          {
-            params: {
-              part: "snippet",
-              playlistId: PLAYLIST_ID,
-              maxResults: 50,
-              key: API_KEY,
-            },
-          }
-        );
-
-        const videosData = response.data.items.map((item) => ({
-          name: item.snippet.title,
-          thumbnail: item.snippet.thumbnails.high.url,
-          src: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
-        }));
-
-        setVideos(videosData);
-      } catch (error) {
-        console.error(
-          "Failed to fetch videos:",
-          error.response?.data || error.message
-        );
-      }
-    };
-
+    // Initial fetch
     fetchVideos();
+
+    // Set up interval to fetch videos every 30 minutes
+    const interval = setInterval(() => {
+      fetchVideos();
+    }, 60 * 60 * 1000); // 60 minutes in milliseconds
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
   }, []);
 
   const handleVideoClick = (vid) => {
